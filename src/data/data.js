bebed0d2b0d63ca986c1fd13498add2873f7802a
cami <malvina.mirtilli@gmail.com>
2022-11-17 15:31:18 -0300

    pancho

--- src/data/data.js
+++ src/data/data.js
@@ -3,5 +3,15 @@ export const foodItem = [
         name: 'Pancho simpatico',
         img: 'img/pancho-1.png',
         section: 'Pancho'
-    }
+    },
+    {
+        name: 'Pancho pulpito',
+        img: 'img/pancho-2.png',
+        section: 'Pancho'
+    },
+    {
+        name: 'Pancho genial',
+        img: 'img/pancho-3.png',
+        section: 'Pancho'
+    },
 ]
