22ce9f586dbf000bcac611fab066dc96506a7050
cami <malvina.mirtilli@gmail.com>
2022-12-05 19:10:35 -0300

    cajkbef

--- src/data/data.js
+++ src/data/data.js
@@ -10,7 +10,7 @@ export const foodItems = [
   {
     id: 2,
     name: 'Pancho Pulpito',
-    img: '/proyecto-integrador/public/img/panchos-2.jpg',
+    img: '/public/img/panchos-2.jpg',
     section: 'Panchos',
     description: 'Pancho con cosas verdes arriba y ketchup.',
     price: 500,
